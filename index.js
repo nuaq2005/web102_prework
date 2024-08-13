fc78bbeca891cdffb606b0c95179164ce4f27d31
Pinamelonade <150365241+Pinamelonade@users.noreply.github.com>
2024-08-13 11:45:28 -0400

    challenge 3 making game card

--- index.js
+++ index.js
@@ -23,32 +23,47 @@ function deleteChildElements(parent) {
 */
 
 // grab the element with the id games-container
-const gamesContainer = document.getElementById("games-container");
+const gamesContainer = document.getElementById('games-container');
 
 // create a function that adds all data from the games array to the page
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
+    for (let i = 0; i < games.length; i++){
 
         // create a new div element, which will become the game card
-
-
+        const gameCard = document.createElement('div');
+        
         // add the class game-card to the list
-
+        gameCard.classList.add('game-card');
 
         // set the inner HTML using a template literal to display some info 
         // about each game
+        const displayInfo = 
+            <div>
+            <h1>${games.name}</h1>
+            <p> ${games.description} </p>
+            <p> Pledged: ${games.pledged} </p>
+            <img src= "games.img" alt ="" />
+        </div>
+        
+        
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
 
+        gameCard.innerHTML = displayInfo;
 
         // append the game to the games-container
+
+        gamesContainer.appendChild(gameCard);
+    }
 
 }
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
+
+addGamesToPage(GAMES_JSON);
 
 
 /*************************************************************************************
